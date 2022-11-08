bc65c1c81929f5bd9a6805e335ff20b49968ed3d
sharmin1988 <rasnasharmin11@gmail.com>
2022-11-08 18:16:29 +0600

    basic setup and api create

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT||5000
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Happy toast server is running , HappyLy!!!!')
+})
+
+
+const uri = `mongodb+srv://${process.env.ADD_USER}:${process.env.ADD_PASSWORD}@cluster0.ex1o8oo.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run (){
+    try{
+        const serviceCollection = client.db('happyToastDb').collection('services')
+
+        app.get('/servicesHome', async(req, res) => {
+            const query = {}
+            const services = await serviceCollection.find(query).limit(3).toArray()
+            res.send(services)
+        })
+        app.get('/services', async(req, res) => {
+            const query = {}
+            const services = await serviceCollection.find(query).toArray()
+            res.send(services)
+        })
+
+    }
+    finally{
+
+    }
+}
+run().catch(err => console.error(err))
+
+
+
+
+app.listen(port, () => {
+    console.log(`server is running on port: ${port}`)
+})
