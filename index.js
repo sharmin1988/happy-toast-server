334633609ad448ff09d665f8f0d41f5f8d28b61d
sharmin1988 <rasnasharmin11@gmail.com>
2022-11-08 20:52:24 +0600

    singel service api add

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT||5000
@@ -31,6 +31,14 @@ async function run (){
             const query = {}
             const services = await serviceCollection.find(query).toArray()
             res.send(services)
+        })
+        app.get('/services/:id', async(req, res) => {
+            const id = req.params.id
+            console.log(id)
+            const query = {_id:ObjectId(id)}
+            const service = await serviceCollection.findOne(query)
+            res.send(service)
+            
         })
 
     }
