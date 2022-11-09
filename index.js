5fe44cf0c416077e1c862e0dabcbc11ba7b20eac
sharmin1988 <rasnasharmin11@gmail.com>
2022-11-09 16:46:55 +0600

    email query added

--- index.js
+++ index.js
@@ -42,38 +42,17 @@ async function run() {
         })
 
         // reviews API
-
-        // app.get('/reviews', async (req, res) => {
-        //     let query = {}
-        //     if (req.query.email) {
-        //         query = {
-        //             email: req.query.email
-        //         }
-        //     }          
-        //     const cursor = reviewCollection.find(query)
-        //     const reviews = await cursor.toArray()
-        //     res.send(reviews)
-        // })
         app.get('/reviews', async (req, res) => {
-            console.log(req.query);
-            const query = {}
-            // if (req.query.service) {
-            //     query = {
-            //         service: 'Greeny Salads'
-            //     }
-            // }
-
+            let query = {}
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
             const cursor = reviewCollection.find(query)
             const reviews = await cursor.toArray()
             res.send(reviews)
         })
-
-        // app.get('/reviews', async(req, res) => {
-        //     const query = {}
-        //     const cursor = reviewCollection.find(query)
-        //     const reviews = await cursor.toArray()
-        //     res.send(reviews)
-        // })
 
         app.post('/reviews', async (req, res) => {
             const reviews = req.body;
